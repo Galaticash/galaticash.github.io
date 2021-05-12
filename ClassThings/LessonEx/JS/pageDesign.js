33e023b9506c7a3a971e07aa51e184e03f37baf5
Galaticash <ash9280@gmail.com>
2021-05-12 11:03:40 -0600

    Catch up
    haven't updated for a while, added lesson 3 and 4

---
+++ ClassThings/LessonEx/JS/pageDesign.js
@@ -0,0 +1,7 @@
+/*
+TODO:
+    add .css file dynamically, could even have a button/selector thing, but basically have the JS change it
+    
+
+
+*/
--- ClassThings/scripts/main.js
+++ ClassThings/scripts/main.js
@@ -4,6 +4,8 @@ var devItems = document.querySelectorAll(".dev");
 for (i = 0; i < devItems.length; i++)
 {
     //console.log(devItems[i]);
+    // would be easier to do this in CSS haha...
+    // but I can comment this out easier so eh
     devItems[i].remove()
 }
 
@@ -11,3 +13,15 @@ document.querySelector('#updateDate').textContent = document.lastModified;
 
 const currentDate = new Date();
 document.querySelector('#year').textContent = currentDate.getFullYear();
+
+/*// when the page is loaded
+window.addEventListener('load', () => {
+    const devButton = document.querySelector("#dev");
+
+    // add the items back to the page, could do a css class thing, is there a way to add css things like display: none in the .js or can you only add classes?
+    devButton.addEventListener('click', () => {
+       
+    }, false);
+
+    //window.onresize = () => {if(window.innerWidth > 760) mainnav.classList.remove('responsive')};
+});*/
